d88acc94fb0ba7df82c78a532a3147de56c64f19
leonkarlsson1 <bang03001@utb.vaxjo.se>
2023-01-20 11:01:56 +0100

    Add project files.

---
+++ Uppgift 5.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Uppgift_5._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal vill du skriva in?");
+
+            int tal = int.Parse(Console.ReadLine());
+
+            int[] antal = new int[tal];
+
+            Console.WriteLine($"Skriv in {tal} heltal:");
+
+            for (int i = 0; i < tal; i++)
+            {
+                antal[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("här är de talen du skrev in:");
+            for(int i = 0; i < antal.Length; i++)   
+            Console.WriteLine(antal[i]);
+
+        }
+    }
+}
